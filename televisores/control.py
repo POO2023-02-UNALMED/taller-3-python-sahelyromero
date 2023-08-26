f6037f552b110e430014ef2d56da29803d710df1
sahelyromero <sahromeroca@unal.edu.co>
2023-08-25 21:18:47 -0500

    intento 2

---
+++ televisores/control.py
@@ -0,0 +1,42 @@
+from televisores.tv import TV
+
+class Control:
+
+    def __init__ (self):
+
+        self.tv
+
+    def turnOn(self):
+        self.tv.turnOn()
+
+    def turnOff(self):
+        self.tv.turnOff()
+
+    def canalUp(self):
+        self.tv.canalUp()
+
+    def canalDown(self):
+        self.tv.canalDown()
+    
+    def volumenUp(self):
+        self.tv.volumenUp()
+
+    def volumenDown(self):
+        self.tv.volumenDown()
+
+    def setCanal(self):
+        self.tv.setCanal()
+
+    def setVolumen(self):
+        self.tv.setVolumen()
+
+    def enlazar(self, televisor):
+
+        self.tv = televisor
+        self.tv.setControl(self)
+
+    def getTv(self):
+        return self.tv
+    
+    def setTv(self, obj):
+        self.tv = obj
---
+++ televisores/marca.py
@@ -0,0 +1,12 @@
+class Marca:
+    def __init__ (self, nombre):
+        
+        self._nombre = nombre
+    
+    def getNombre(self):
+        
+        return self.nombre
+    
+    def setNombre(self, nombre):
+
+        self.nombre = nombre
